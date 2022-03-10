3cff71a14b3c0699fcedfc7c01ab32ba638ffaba
Sunil Tadepalli <sunil.tadepalli@qlinics.com>
2022-03-10 17:29:16 +0530

    added more patients to list

--- lib/resources/patient_resource.dart
+++ lib/resources/patient_resource.dart
@@ -150,7 +150,7 @@ List<Patient> patients = [
         name: HumanName(
           family: 'Sharma',
           given: [
-            'ohit',
+            'Mohit',
           ],
         ),
         telecom: <ContactPoint>[
@@ -161,7 +161,90 @@ List<Patient> patients = [
           ),
           ContactPoint(
             system: ContactPointSystem.email,
-            value: 'anirudh.jadeja@gmail.com',
+            value: 'mohit.sharma@gmail.com',
+            rank: PositiveInt(2),
+          ),
+        ],
+      )
+    ],
+  ),
+  Patient(
+    name: <HumanName>[
+      HumanName(
+        family: 'Kohli',
+        given: [
+          'Virat',
+        ],
+      ),
+    ],
+    birthDate: Date(DateTime(1988, 11, 5)),
+    gender: PatientGender.male,
+    address: <Address>[
+      Address(
+        text: 'Royal Challenger',
+        city: 'Bengaluru',
+        state: 'Karnataka',
+        country: 'India',
+        postalCode: '560001',
+      )
+    ],
+    identifier: <Identifier>[
+      Identifier(
+        use: IdentifierUse.official,
+        system: FhirUri('https://healthid.ndhm.gov.in'),
+        value: 'viratkohli@ndhm',
+      ),
+      Identifier(
+        use: IdentifierUse.official,
+        system: FhirUri('https://healthid.ndhm.gov.in'),
+        value: '12345678912345', // 14-digit-number
+      ),
+      Identifier(
+        use: IdentifierUse.official,
+        system: FhirUri('https://uidai.gov.in'),
+        value: '123412341234',
+      ), // 12-digit- aadhar-number,
+    ],
+    telecom: <ContactPoint>[
+      ContactPoint(
+        system: ContactPointSystem.phone,
+        value: '+919810012345', //Mobile Number
+        rank: PositiveInt(1),
+      ),
+      ContactPoint(
+        system: ContactPointSystem.email,
+        value: 'virat.kohli@gmail.com',
+        rank: PositiveInt(2),
+      ),
+    ],
+    contact: <PatientContact>[
+      PatientContact(
+        relationship: [
+          CodeableConcept(
+            coding: [
+              Coding(
+                system: FhirUri('http://snomed.info/sct'),
+                code: Code('66839005'),
+                display: 'Father',
+              ),
+            ],
+          ),
+        ],
+        name: HumanName(
+          family: 'Kohli',
+          given: [
+            'Prem',
+          ],
+        ),
+        telecom: <ContactPoint>[
+          ContactPoint(
+            system: ContactPointSystem.phone,
+            value: '+919810054321',
+            rank: PositiveInt(1),
+          ),
+          ContactPoint(
+            system: ContactPointSystem.email,
+            value: 'prem.kohli@gmail.com',
             rank: PositiveInt(2),
           ),
         ],
